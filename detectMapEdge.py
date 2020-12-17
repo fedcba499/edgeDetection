a2952fbf5aa298741419e1e01a8e6dfd28e613a4
fedcba499 <72005953+fedcba499@users.noreply.github.com>
2020-12-17 18:49:32 +0530

    version 3 changes

--- detectMapEdge.py
+++ detectMapEdge.py
@@ -2,52 +2,58 @@ import cv2
 
 import numpy as np
 
-# Image noise is random variation of brightness or color information in images,
-#  and is usually an aspect of electronic noise. 
-# It can be produced by the image sensor and circuitry of a scanner or digital camera.
-#  ... Image noise is an undesirable by-product of image 
-# capture that obscures the desired information.
+import mapper
 
-img2 = cv2.imread("nc-43-03.jpg")
-
+img2 = cv2.imread("nd-44-10.jpg")
 
 img2 = img2[0:3350, 0:5000]
 
 #resize image to 1:5
 
-
 img = cv2.resize(img2, (0,0), fx=0.2, fy=0.2)
-
-# img1 = cv2.blur(img, (3,3))
-# contrastImg = cv2.addWeighted(img1,1.5, np.zeros(img.shape, img.dtype), 0, 0)
-
-# contrastImg2 = cv2.addWeighted(img,1.5, np.zeros(img.shape, img.dtype), 0, 0)
-# img2 = cv2.blur(contrastImg2, (3,3))
-
-
 
 result = cv2.fastNlMeansDenoisingColored(img,None,20,10,7,21)
 
-result1 = cv2.fastNlMeansDenoisingColored(img,None,35,10,7,21)
-
 edgeImg = cv2.Canny(result, 100,200)
 
-edgeImg1 = cv2.Canny(result1, 100, 200)
+contours,hierarchy=cv2.findContours(edgeImg,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)  
+#retrieve the contours as a list, with simple apprximation model
 
-## find the non-zero min-max coords of canny
-pts = np.argwhere(edgeImg1>0)
-y1,x1 = pts.min(axis=0)
-y2,x2 = pts.max(axis=0)
+contours=sorted(contours,key=cv2.contourArea,reverse=True)
+#the loop extracts the boundary contours of the page
 
-## crop the region
-# cropped = img2[y1*5:y2*5, x1*5:x2*5]
-# cv2.imwrite("cropped3.png", cropped)
+for c in contours:
+    p=cv2.arcLength(c,True)
+    approx=cv2.approxPolyDP(c,0.02*p,True)
 
-# cv2.imshow("original img", img)
-# cv2.imshow("original contrast img", contrast_img)
-# cv2.imshow("noise reduced img", result)
-# cv2.imshow("noise reduced contrast img", result1)
-cv2.imshow("low str noise", edgeImg)
-cv2.imshow("high str noise", edgeImg1)
+    if len(approx)==4:
+        target=approx
+        break
+approx=mapper.mapp(target)
+ #find endpoints of the sheet
+
+approx1 = [[(j*5) for j in i] for i in approx]
+#scale to get original pts
+
+approx1 = np.float32(approx1)
+
+pts=np.float32([[0,0],[3600,0],[3600,2400],[0,2400]])  
+#map to 800*800 target window
+
+op=cv2.getPerspectiveTransform(approx1,pts)  
+#get the top or bird eye view effect
+
+dst=cv2.warpPerspective(img2,op,(3600,2400))
+
+
+# cv2.imshow("Scanned",dst)
+
+cv2.imwrite("nd-44-10.png", dst)
+
+
+# cv2.imshow("edge detected", edgeImg)
+
+# print(approx)
+# print(approx1)
 
 cv2.waitKey(0)
